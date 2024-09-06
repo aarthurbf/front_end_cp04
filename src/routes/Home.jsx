216c03d01e8773f593d0f550f31892e5d6385f90
Arthur Bobadilla Franchi <arthurbobadilla2006@gmail.com>
2024-09-06 18:47:17 -0300

    Add: Banner text

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,27 +6,27 @@ function Home() {
     <>
       {/* BANNER */}
       <section className="banner">
-        <h2>Venha conhecer o paraíso</h2>
+        <h2>Venha conhecer os nossos produtos</h2>
         <h3>Lorem ipsum dolor sit amet.</h3>
         <a href="" className="btn">Informações</a>
       </section>
 
       {/* SERVIÇOS */}
       <section className="informacoes">
-        <h3>Lorem ipsum dolor sit amet.</h3>
+        <h3>Vantagens</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, velit.</p>
         <hr/>
           <ul className="item">
             <li>
-              <h4>Localização</h4>
+              <h4>Entrega</h4>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit cupiditate atque aspernatur quam vel amet.</p>
             </li>
             <li>
-              <h4>Pacotes e ofertas</h4>
+              <h4>Ofertas</h4>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit cupiditate atque aspernatur quam vel amet.</p>
             </li>
             <li>
-              <h4>Acomodações</h4>
+              <h4>Variedade</h4>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit cupiditate atque aspernatur quam vel amet.</p>
             </li>
           </ul>
