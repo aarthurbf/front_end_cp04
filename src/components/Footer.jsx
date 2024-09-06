bdd87717dcced9cbb4d24939701e0795caf97924
Arthur Bobadilla Franchi <arthurbobadilla2006@gmail.com>
2024-09-06 17:46:44 -0300

    Arthur Add: Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,20 @@
+import "../css/Footer.css"
+
 function Footer() {
 
-    return (
-      <>
-        
-      </>
-    )
-  }
-  
-  export default Footer
+  return (
+    <>
+      <section>
+        <ul>
+          <li><a href=""><FontAwesomeIcon icon="fa-brands fa-square-instagram" /></a></li>
+          <li><a href=""><FontAwesomeIcon icon="fa-brands fa-square-facebook" /></a></li>
+          <li><a href=""><FontAwesomeIcon icon="fa-brands fa-square-x-twitter" /></a></li>
+          <li><a href=""><FontAwesomeIcon icon="fa-brands fa-square-whatsapp" /></a></li>
+        </ul>
+        <p>Todos os direitos reservados</p>
+      </section>
+    </>
+  )
+}
+
+export default Footer
