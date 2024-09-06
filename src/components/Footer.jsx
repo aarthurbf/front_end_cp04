2a21727ad9bc7c8348d8329e12c1c8cb59be81f4
Arthur Bobadilla Franchi <arthurbobadilla2006@gmail.com>
2024-09-06 18:05:26 -0300

    Fix: Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,6 @@ function Footer() {
   return (
     <>
       <section>
-        <ul>
-          <li><a href=""><FontAwesomeIcon icon="fa-brands fa-square-instagram" /></a></li>
-          <li><a href=""><FontAwesomeIcon icon="fa-brands fa-square-facebook" /></a></li>
-          <li><a href=""><FontAwesomeIcon icon="fa-brands fa-square-x-twitter" /></a></li>
-          <li><a href=""><FontAwesomeIcon icon="fa-brands fa-square-whatsapp" /></a></li>
-        </ul>
         <p>Todos os direitos reservados</p>
       </section>
     </>
