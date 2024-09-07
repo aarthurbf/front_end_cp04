387a4300d824c802019c0437c4a09e3e92ba9cd9
Arthur Bobadilla Franchi <arthurbobadilla2006@gmail.com>
2024-09-06 23:54:47 -0300

    Add: Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./routes/Home";
-import Produtos from "./routes/Produtos";
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Home from './routes/Home';
+import Produtos from './routes/Produtos';
+import Contato from './routes/Contato';
+import Sobre from './routes/Sobre';
 import "./css/Global.css";
 
 function App() {
+    const [currentPage, setCurrentPage] = useState('home');
 
-  return (
-    <>
-      <Header/>
-      <Home/>
-      <Produtos/>
-      <Footer/>
-    </>
-  )
+    const renderPage = () => {
+        switch (currentPage) {
+            case 'home':
+                return <Home />;
+            case 'produtos':
+                return <Produtos />;
+            case 'contato':
+                return <Contato />;
+            case 'sobre':
+                return <Sobre />;
+            default:
+                return <Home />;
+        }
+    };
+
+    return (
+        <div>
+            <Header onNavigate={setCurrentPage} />
+            <main>
+                {renderPage()}
+            </main>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,21 @@
-import "../css/Header.css"
-function Header() {
+import "../css/Header.css";
 
+function Header({ onNavigate }) {
     return (
         <>
             <section className="header">
-                <h1><a href="">GoAthlete</a></h1>
-                <nav class="header-menu">
+                <h1><a href="#" onClick={() => onNavigate('home')}>GoAthlete</a></h1>
+                <nav className="header-menu">
                     <ul>
-                        <li><a href="">Home</a></li>
-                        <li><a href="">Produtos</a></li>
-                        <li><a href="">Contato</a></li>
-                        <li><a href="">Sobre</a></li>
+                        <li><a href="#" onClick={() => onNavigate('home')}>Home</a></li>
+                        <li><a href="#" onClick={() => onNavigate('produtos')}>Produtos</a></li>
+                        <li><a href="#" onClick={() => onNavigate('contato')}>Contato</a></li>
+                        <li><a href="#" onClick={() => onNavigate('sobre')}>Sobre</a></li>
                     </ul>
                 </nav>
             </section>
         </>
-    )
+    );
 }
 
 export default Header;
