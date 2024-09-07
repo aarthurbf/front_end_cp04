d99af67951c219a9e88489cb6c5f09a164970f5b
Arthur Bobadilla Franchi <arthurbobadilla2006@gmail.com>
2024-09-07 00:31:44 -0300

    Add:Contato

--- src/routes/Contato.jsx
+++ src/routes/Contato.jsx
@@ -1,10 +1,20 @@
+import "../css/Contato.css"
 function Contato() {
 
-    return (
-      <>
-        
-      </>
-    )
-  }
-  
-  export default Contato;
+  return (
+    <>
+      {/* CADASTRO */}
+      <section className="cadastro">
+        <h3>Entre em contato conosco</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti suscipit, commodi quo molestias sapiente optio est amet aliquid reprehenderit explicabo?</p>
+
+        <form name="cadastro" id="cadastro" method="post" action="">
+          <input type="text" placeholder="Sua mensagem"/>
+            <button type="submit" name="button">Enviar</button>
+        </form>
+      </section>
+    </>
+  )
+}
+
+export default Contato;
