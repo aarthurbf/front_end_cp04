f2cb66c1b8e6a5ac62ecaff86d2c096637234403
Arthur Bobadilla Franchi <arthurbobadilla2006@gmail.com>
2024-09-07 13:45:22 -0300

    Add: Avaliações

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,6 +2,10 @@ import "../css/Home.css";
 import EntregaIcon from "../assets/estoque-pronto.png"
 import VariedadeIcon from "../assets/caixas.png"
 import DescontoIcon from "../assets/desconto.png"
+import Estrela from "../assets/star.png"
+import FaceOne from "../assets/face1.jpg"
+import FaceTwo from "../assets/face2.jpg"
+import FaceThree from "../assets/face3.jpg"
 
 function Home() {
 
@@ -36,6 +40,66 @@ function Home() {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit cupiditate atque aspernatur quam vel amet.</p>
           </li>
         </ul>
+      </section>
+
+      {/* AVALIAÇÃO */}
+      <section className="testimonials">
+        <div className="inner">
+          <h1>Avaliações</h1>
+          <div className="border"></div>
+          <div className="row">
+            <div className="col">
+              <div className="testimonial">
+                <img src={FaceOne} alt="" />
+                <div className="name">Júlia</div>
+                <div className="stars">
+                  <img src={Estrela} alt="" />
+                  <img src={Estrela} alt="" />
+                  <img src={Estrela} alt="" />
+                  <img src={Estrela} alt="" />
+                  <img src={Estrela} alt="" />
+                </div>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                </p>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="testimonial">
+                <img src={FaceTwo} alt="" />
+                <div className="name">Roberto</div>
+                <div className="stars">
+                  <img src={Estrela} alt="" />
+                  <img src={Estrela} alt="" />
+                  <img src={Estrela} alt="" />
+                  <img src={Estrela} alt="" />
+                  <img src={Estrela} alt="" />
+                </div>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                </p>
+              </div>
+            </div>
+
+            <div className="col">
+              <div className="testimonial">
+                <img src={FaceThree} alt="" />
+                <div className="name">Lucas</div>
+                <div className="stars">
+                  <img src={Estrela} alt="" />
+                  <img src={Estrela} alt="" />
+                  <img src={Estrela} alt="" />
+                  <img src={Estrela} alt="" />
+                  <img src={Estrela} alt="" />
+                </div>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   )
